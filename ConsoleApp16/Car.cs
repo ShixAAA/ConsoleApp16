b77f1d83898a71f3b423301142f30d1a2c018d2d
test05 <test5@user.com>
2022-03-28 13:54:21 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp16/Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    internal class Car
+    {
+        public string Model { get; set; }
+        public string Number { get; set; }
+        public Car() {}
+        public Car(string model, string number)
+        {
+            Model = model;
+            Number = number;
+        }
+        public virtual void Show() => Console.WriteLine($"Car: Model = {Model},Number = {Number}");
+        
+    }
+    internal class Sedan: Car
+    {
+
+        int NumberOfPlaces;
+        public Sedan(string Model, string Number, int NumberOFPlaces) : base(Model, Number)
+        {
+            NumberOfPlaces = NumberOFPlaces;
+        }
+        public override void Show() => Console.WriteLine($"Легковой автомобиль: Марка - {Model}, Номер - {Number}, Число пассажирских мест - {NumberOfPlaces}");
+    }
+    internal class Cargo: Car
+    {
+        int LoadCapacity;
+        public Cargo(string Model, string Number, int LoadCapacity) : base(Model, Number)
+        {
+            this.LoadCapacity = LoadCapacity;
+        }
+        public override void Show() => Console.WriteLine($"Грузовой автомобиль: Марка - {Model}, Номер - {Number}, Грузоподъемность - {LoadCapacity}т");
+    }
+}   
---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sedan s = new Sedan("Renault","В591РС777",5);
+            s.Show();
+            Cargo c = new Cargo("DAF", "О206КМ199", 55);
+            c.Show();
+            Console.ReadKey();
+        }
+    }
+}
